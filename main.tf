b0a64f73639759c216604933b00bf396a7c51e42
Rounak <s.rounak@outlook.com>
2025-09-25 21:02:09 +0100

    1st commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.12.0"
+    }
+  }
+
+  required_version = ">= 1.9.0"
+}
+# configures the provider
+
+provider "azurerm" {
+  features {}
+}
